0f32efa688ad697fa4f5a6fe3ec7ec149f51ced6
Alexsandro <sandrobass@hotmail.com>
2019-12-07 19:33:08 +0000

    upgrate

---
+++ conta/tests.py
@@ -0,0 +1,32 @@
+from django.test import TestCase
+
+'''Upload documents on Github
+
+git clone <nome>
+
+<entra na pasta criada>
+
+git add .
+
+git commit -m "texto"
+
+git push
+'''
+
+
+#Heroku
+#https://github.com/Gpzim98/django-heroku
+
+#git add .gitignore
+#colocar no gitignore
+'''.idea
+.pyc
+.DS_Store
+*.sqlite3'''
+
+'''
+Publishing the app
+git add .
+git commit -m "Configuring the app"
+git push heroku master --force
+'''
---
+++ controlegastos/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('conta.urls')),
+    #path('accounts/', include('conta.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+
+]
